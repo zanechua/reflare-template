910b6217d0c55ba0fd9b059ef22906c92811a8b4
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-04-29 12:05:27 +0800

    Init the template with a basic script file
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import RocketBooster from 'rocket-booster';
+
+const config = {
+  basic: {
+    upstream: 'https://en.wikipedia.org/',
+    mobileRedirect: 'https://en.m.wikipedia.org/',
+  },
+
+  firewall: {
+    blockedRegion: ['CN', 'KP', 'SY', 'PK', 'CU'],
+    blockedIPAddress: [],
+    scrapeShield: true,
+  },
+
+  routes: {
+    TW: 'https://zh.wikipedia.org/',
+    HK: 'https://zh.wikipedia.org/',
+    FR: 'https://fr.wikipedia.org/',
+  },
+
+  optimization: {
+    cacheEverything: false,
+    cacheTtl: 5,
+    mirage: true,
+    polish: 'off',
+    minify: {
+      javascript: true,
+      css: true,
+      html: true,
+    },
+  },
+};
+
+addEventListener('fetch', (event) => {
+  const booster = new RocketBooster(config);
+  const response = booster.apply(event.request);
+  event.respondWith(response);
+});
